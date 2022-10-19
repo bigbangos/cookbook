cf694d5f502637cf4b9dabd4eb0e1a4f0773f5a8
Samuel Oloruntoba <5002506+kayandra@users.noreply.github.com>
2022-10-19 22:01:21 +0100

    feat: custom callout component

--- src/components/MDXComponents.jsx
+++ src/components/MDXComponents.jsx
@@ -1,6 +1,7 @@
 import { onlyText } from 'react-children-utilities'
 
 // local imports
+import { Callout } from '@/components/Callout'
 import { Fence } from '@/components/Fence'
 
 export const getComponents = () => ({
@@ -8,4 +9,7 @@ export const getComponents = () => ({
   code: function ({ children, ...args }) {
     return <Fence language={args['data-language']}>{onlyText(children)}</Fence>
   },
+
+  // custom components
+  Callout,
 })
